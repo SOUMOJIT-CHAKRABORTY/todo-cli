cc56460301e99bd61d52ae5d6d413939b4792b92
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-11-17 23:17:11 +0530

    completed and delete todo function added

--- cmd/todo.go
+++ cmd/todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type item struct {
 	Task        string
@@ -21,4 +24,31 @@ func (t *Todos) Add(task string) {
 	}
 
 	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+
+	if index < 0 || index >= len(ls) {
+		return errors.New("Invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+
+	if index < 0 || index >= len(ls) {
+		return errors.New(("Invalid index"))
+	}
+
+	// First argument to append must be original slice; have []item {form 0th index to index-2, excluding index-1}
+	// Second argument to append must be new elements to be added; {form index to last index}
+	*t = append(ls[:index-1], ls[index:]...)
+
+	return nil
 }
